22e7efe6eb315830d96fc7add8420c1966e5d236
Anthony Cuevas <acuevas.15@gmail.com>
2018-02-02 23:46:10 -0500

    fixed padding

--- insertion_sort.h
+++ insertion_sort.h
@@ -2,9 +2,9 @@
     typeof(*keys) inserting = keys[from]; \
     while(from >= to) { \
         typeof(*keys) moving = keys[to]; \
-	    keys[to] = inserting; \
-	    inserting = moving; \
-	    to++; \
+        keys[to] = inserting;            \
+        inserting = moving;              \
+        to++;                            \
     } \
 } while(0)
 
